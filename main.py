87e0957a35487c3137a3bd5dae83b8ca9fb3e67b
BorgRect <code10learn@gmail.com>
2019-11-05 07:32:21 -0600

    Color and speed have been defined for all circles and now using a circle instead of drawing one for each color in the rainbow.  Added DrawSpline class and set most of the init properties.

--- main.py
+++ main.py
@@ -14,6 +14,25 @@ BLUE = (0, 0, 255)
 INDIGO = (75, 0, 130)
 VIOLET = (255, 0, 255)
 
+class DrawSpline:
+    def __init__(self, colorX, ColorY, speedX, speedY, coordsX, coordsY,  next_tick):
+        self.color = colorX
+        self.speed = (speedX + speedY) / 2
+        self.coords = coordsX, coordsY
+        self.rect = pygame.Rect(self.coords[0],self.coords[1], 20, 20)
+        self.points = []
+        self.next_tick = next_tick
+        self.angle_init = self.angle
+        self.coords_init = self.coords
+        self.points.append([int(self.coords[0]), int(self.coords[1])])
+
+
+
+
+
+
+
+
 class DrawCircle:
     def __init__(self, color, speed, coords, angle, radius, next_tick):
         self.color = color
@@ -50,8 +69,10 @@ class DrawCircle:
         # print(self.coords)
 
     def draw_class(self, screen):
-        pygame.draw.lines(screen, self.color, False, self.points, 1)
+        # pygame.draw.lines(screen, self.color, False, self.points, 1)
+        pygame.draw.circle(screen, self.color, [int(self.coords_init[0] - 30), int(self.coords_init[1])], 30, 1)
         pygame.draw.circle(screen, WHITE, [int(self.coords[0]), int(self.coords[1])], 4)
+
 
 def move_coords(angle, radius, coords):
     theta = math.radians(angle)
@@ -65,7 +86,7 @@ def drawHouse(x, y, width, height, screen, color):
 
 def main():
     pygame.display.set_caption("Oribit")
-    screen = pygame.display.set_mode((800, 800))
+    screen = pygame.display.set_mode((700, 700))
     clock = pygame.time.Clock()
     vert_circles = []
     RainBow = []
@@ -77,13 +98,24 @@ def main():
     RainBow.append(INDIGO)
     RainBow.append(VIOLET)
     test_coords = 100, 100
+
     for i in range(7):
-        test_coords = 78, 100 + (78 * i)
-        speed = 30 
+        test_coords = 78, 110 + (78 * i)
+        speed = 30 / (i + 1)
         vert_circles.append(DrawCircle(RainBow[i], speed, test_coords, 90, 0.5, 1))
+        # test_coords = 150, 110 + (78 * i)
+        # vert_circles.append(DrawCircle(RainBow[i], speed, test_coords, 90, 0.5, 1))
+
     for i in range(7):
         test_coords = 150 + (78 * i), 39
-        vert_circles.append(DrawCircle(RainBow[i], 1, test_coords, 90, 0.5, 1))
+        speed = 30 / (i + 1)
+        vert_circles.append(DrawCircle(RainBow[i], speed, test_coords, 90, 0.5, 1))
+
+    # for i in range(7):
+    #     for j in range(7):
+    #         test_coords = 150 + (78 * i), 110 + (78 * j)
+    #         speed = 30 / (i + 1)
+    #         vert_circles.append(DrawCircle(RainBow[i], speed, test_coords, 90, 0.5, 1))
 
 
 
@@ -113,7 +145,7 @@ def main():
             vert_circles[i].draw_class(screen)
 
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(360)
 
 
     pygame.quit()
