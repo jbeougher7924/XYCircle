9a365ead394f59722410b17d13c396f38554a1d0
BorgRect <code10learn@gmail.com>
2019-11-05 21:19:26 -0600

    Its now drawling the splines based on the vert and horizontal circles, however the splins are drawn in mirror fashion.  This needs to be corrected.

--- main.py
+++ main.py
@@ -1,51 +1,77 @@
 import pygame
 import math
 
+BLACK = [0, 0, 0]
+WHITE = [255, 255, 255]
 
-BLACK = (  0,   0,   0)
-WHITE = (255, 255, 255)
+RED = [255, 0, 0]
+ORANGE = [255, 128, 0]
+YELLOW = [255, 255, 0]
+GREEN = [0, 255, 0]
+BLUE = [0, 0, 255]
+INDIGO = [75, 0, 130]
+VIOLET = [255, 0, 255]
 
-
-RED = (255, 0, 0)
-ORANGE = (255, 128, 0)
-YELLOW = (255, 255, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-INDIGO = (75, 0, 130)
-VIOLET = (255, 0, 255)
 
 class DrawSpline:
-    def __init__(self, colorX, ColorY, speedX, speedY, coordsX, coordsY,  next_tick):
-        self.color = colorX
+    shared_coords = {}
+
+    def __init__(self, colorX, colorY, speedX, speedY, coordsX, coordsY, indexX, indexY, next_tick):
+        self.color = [(colorX[0] + colorY[0]) / 2, (colorX[1] + colorY[1]) / 2, (colorX[2] + colorY[2]) / 2]
         self.speed = (speedX + speedY) / 2
         self.coords = coordsX, coordsY
-        self.rect = pygame.Rect(self.coords[0],self.coords[1], 20, 20)
+        self.rect = pygame.Rect(self.coords[0], self.coords[1], 20, 20)
         self.points = []
         self.next_tick = next_tick
-        self.angle_init = self.angle
         self.coords_init = self.coords
+        self.indexX = indexX
+        self.indexY = indexY
         self.points.append([int(self.coords[0]), int(self.coords[1])])
 
+    def add_points(self, ticks, reset):
+        if ticks > self.next_tick:
+            self.next_tick += self.speed
+            # print("X: {} Y: {}".format(self.indexX, self.indexY))
+            # print(self.shared_coords)
+            xCoords = self.shared_coords[self.indexX]
+            yCoords = self.shared_coords[self.indexY]
+            self.coords = self.coords_init[0] - xCoords[0] + 19, yCoords[1]
+            self.rect.topleft = self.coords
+            self.points.append([int(self.coords[0]), int(self.coords[1])])
 
+        if reset:
+            self.coords = self.coords_init
+            temp = []
+            temp.append(self.points[1])
+            self.points.clear()
+            self.points.append([int(self.coords[0]), int(self.coords[1])])
+            self.points.append(temp[0])
 
+    def draw_class(self, screen):
+        pygame.draw.lines(screen, self.color, False, self.points, 1)
+        # pygame.draw.circle(screen, self.color, [int(self.coords_init[0] - 30), int(self.coords_init[1])], 30, 1)
 
-
-
+    def put_shared_coords(self, shared_coords):
+        self.shared_coords = shared_coords
 
 
 class DrawCircle:
-    def __init__(self, color, speed, coords, angle, radius, next_tick):
+    shared_coords = {}
+
+    def __init__(self, color, speed, coords, angle, radius, next_tick, index):
         self.color = color
         self.speed = speed
         self.coords = coords
         self.angle = angle
         self.radius = radius
-        self.rect = pygame.Rect(self.coords[0],self.coords[1], 20, 20)
+        self.rect = pygame.Rect(self.coords[0], self.coords[1], 20, 20)
         self.points = []
         self.next_tick = next_tick
         self.angle_init = self.angle
         self.coords_init = self.coords
         self.points.append([int(coords[0]), int(coords[1])])
+        self.index = index
+        self.shared_coords[self.index] = self.coords
 
     def add_points(self, ticks):
         if ticks > self.next_tick:
@@ -53,15 +79,20 @@ class DrawCircle:
             self.angle += 1
             self.coords = move_coords(self.angle, self.radius, self.coords)
             self.rect.topleft = self.coords
-            self.points.append([int(self.coords[0]), int (self.coords[1])])
+            self.points.append([int(self.coords[0]), int(self.coords[1])])
+            self.shared_coords[self.index] = self.coords
 
-        if self.points[-1] == [self.coords_init[0] -1, self.coords_init[1] -1]:
+        if self.points[-1] == [self.coords_init[0] - 1, self.coords_init[1] - 1]:
             self.coords = self.coords_init
             self.angle = self.angle_init
             self.points.clear()
-            self.points.append([int(self.coords[0]), int (self.coords[1])])
+            self.points.append([int(self.coords[0]), int(self.coords[1])])
             self.coords = move_coords(self.angle, self.radius, self.coords)
-            self.points.append([int(self.coords[0]), int (self.coords[1])])
+            self.points.append([int(self.coords[0]), int(self.coords[1])])
+            if self.index == 0:
+                return True
+
+        return False
 
         # print("points[-1]")
         # print(self.points[-1])
@@ -73,22 +104,28 @@ class DrawCircle:
         pygame.draw.circle(screen, self.color, [int(self.coords_init[0] - 30), int(self.coords_init[1])], 30, 1)
         pygame.draw.circle(screen, WHITE, [int(self.coords[0]), int(self.coords[1])], 4)
 
+    def get_shared_coords(self):
+        return self.shared_coords
+
 
 def move_coords(angle, radius, coords):
     theta = math.radians(angle)
     return coords[0] + radius * math.cos(theta), coords[1] + radius * math.sin(theta)
 
+
 def drawHouse(x, y, width, height, screen, color):
-    points = [(x,y- ((2/3.0) * height)), (x,y), (x+width,y), (x+width,y-(2/3.0) * height),
-        (x,y- ((2/3.0) * height)), (x + width/2.0,y-height), (x+width,y-(2/3.0)*height)]
+    points = [(x, y - ((2 / 3.0) * height)), (x, y), (x + width, y), (x + width, y - (2 / 3.0) * height),
+              (x, y - ((2 / 3.0) * height)), (x + width / 2.0, y - height), (x + width, y - (2 / 3.0) * height)]
     lineThickness = 2
     pygame.draw.lines(screen, color, False, points, lineThickness)
+
 
 def main():
     pygame.display.set_caption("Oribit")
     screen = pygame.display.set_mode((700, 700))
     clock = pygame.time.Clock()
     vert_circles = []
+    splines = []
     RainBow = []
     RainBow.append(RED)
     RainBow.append(ORANGE)
@@ -97,19 +134,26 @@ def main():
     RainBow.append(BLUE)
     RainBow.append(INDIGO)
     RainBow.append(VIOLET)
-    test_coords = 100, 100
 
     for i in range(7):
         test_coords = 78, 110 + (78 * i)
         speed = 30 / (i + 1)
-        vert_circles.append(DrawCircle(RainBow[i], speed, test_coords, 90, 0.5, 1))
+        vert_circles.append(DrawCircle(RainBow[i], speed, test_coords, 90, 0.5, 1, i))
         # test_coords = 150, 110 + (78 * i)
         # vert_circles.append(DrawCircle(RainBow[i], speed, test_coords, 90, 0.5, 1))
 
     for i in range(7):
         test_coords = 150 + (78 * i), 39
         speed = 30 / (i + 1)
-        vert_circles.append(DrawCircle(RainBow[i], speed, test_coords, 90, 0.5, 1))
+        vert_circles.append(DrawCircle(RainBow[i], speed, test_coords, 90, 0.5, 1, i + 7))
+
+    for x in range(7):
+        xCoords = 150 + (78 * x)
+        for y in range(7):
+            speedX = 30 / (x + 1)
+            speedY = 30 / (y + 1)
+            yCoords = 110 + (78 * y)
+            splines.append(DrawSpline(RainBow[x], RainBow[y], speedX, speedY, xCoords, yCoords, x, y, 1))
 
     # for i in range(7):
     #     for j in range(7):
@@ -117,10 +161,8 @@ def main():
     #         speed = 30 / (i + 1)
     #         vert_circles.append(DrawCircle(RainBow[i], speed, test_coords, 90, 0.5, 1))
 
-
-
-
     running = True
+    reset = []
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -131,22 +173,24 @@ def main():
                 running = False
 
         ticks = pygame.time.get_ticks()
-        for i in range(len(vert_circles)):
-            vert_circles[i].add_points(ticks)
 
+        for vert_circle in vert_circles:
+            reset.append( vert_circle.add_points(ticks))
 
-
-
+        for spline in splines:
+            spline.put_shared_coords(vert_circles[0].get_shared_coords())
+            spline.add_points(ticks, reset[0])
 
         screen.fill((0, 0, 30))
 
+        for vert_circle in vert_circles:
+            vert_circle.draw_class(screen)
 
-        for i in range(len(vert_circles)):
-            vert_circles[i].draw_class(screen)
-
+        for spline in splines:
+            spline.draw_class(screen)
+        reset.clear()
         pygame.display.flip()
         clock.tick(360)
-
 
     pygame.quit()
 
